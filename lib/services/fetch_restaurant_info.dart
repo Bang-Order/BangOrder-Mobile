917bc6935e1e1c03cf5061d0f3916ea6b2667df6
tutdeputraw <anoboy.code@gmail.com>
2021-10-18 22:56:03 +0700

    fix bug

--- lib/services/fetch_restaurant_info.dart
+++ lib/services/fetch_restaurant_info.dart
@@ -1,11 +1,10 @@
 part of 'services.dart';
 
 Future <RestaurantInfo> fetchRestaurantInfo() async {
-  final url = APIURL + 'restaurants/2';
+  final url = APIURL + 'restaurants/1';
   final response = await http.get(Uri.parse(url));
 
   if (response.statusCode == 200) {
-    print(response.body);
     return RestaurantInfo.fromJson(jsonDecode(response.body));
   } else {
     throw Exception('Failed to load data'+ response.statusCode.toString());
