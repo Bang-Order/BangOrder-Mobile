dfd4351689594807f8c7f1d9b15a9071c1e0671b
tutdeputraw <anoboy.code@gmail.com>
2021-12-21 10:21:16 +0700

    enhance empty order history layout

--- lib/ui/pages/order_history_page/order_history_page_view.dart
+++ lib/ui/pages/order_history_page/order_history_page_view.dart
@@ -25,40 +25,69 @@ class OrderHistoryPage extends StatelessWidget {
             ? Center(child: CircularProgressIndicator())
             : controller.orderHistory.isNotEmpty
                 ? SingleChildScrollView(
-                    child: Container(
+                    child: ListView.separated(
                       padding: EdgeInsets.symmetric(
                         vertical: defaultMargin / 2,
                       ),
-                      child: ListView.separated(
-                        shrinkWrap: true,
-                        controller: ScrollController(),
-                        itemCount: controller.orderHistory.length,
-                        itemBuilder: (context, index) => HistoryCard(
-                          data: controller.orderHistory[index],
-                          // data: snapshot.data![index],
-                        ),
-                        separatorBuilder: (context, index) => Divider(
-                          color: Colors.black,
-                        ),
+                      shrinkWrap: true,
+                      controller: ScrollController(),
+                      itemCount: controller.orderHistory.length,
+                      itemBuilder: (context, index) => HistoryCard(
+                        data: controller.orderHistory[index],
+                        // data: snapshot.data![index],
+                      ),
+                      separatorBuilder: (context, index) => Divider(
+                        color: Colors.black,
                       ),
                     ),
                   )
-                : Center(
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Lottie.asset(
-                          'assets/images/empty_history.json',
-                          width: MediaQuery.of(context).size.width,
-                          height: MediaQuery.of(context).size.height * 0.35,
-                        ),
-                        ElevatedButton(
-                          onPressed: () => Get.off(ScanQrPage(),duration: Duration(milliseconds: 500)),
-                          child: Text('Silahkan Order'),
-                        )
-                      ],
-                    ),
-                  ),
+                : haveNotOrderedYet,
+      ),
+    );
+  }
+
+  Widget get haveNotOrderedYet {
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Container(
+            padding: EdgeInsets.all(16),
+            child: Text(
+              'Belum Ada Riwayat Pesanan',
+              textAlign: TextAlign.center,
+              style: orderListStyle,
+            ),
+          ),
+          Container(
+            padding: EdgeInsets.only(
+              left: 16,
+              right: 16,
+              bottom: 32,
+            ),
+            child: Text(
+              'Silahkan pesan terlebih dahulu',
+              textAlign: TextAlign.center,
+              style: successPopupTitle,
+            ),
+          ),
+          Lottie.asset(
+            'assets/images/empty_history.json',
+            width: Get.width,
+          ),
+          Container(
+            margin: EdgeInsets.only(top: 16),
+            padding: EdgeInsets.all(16),
+            child: ElevatedButton(
+              onPressed: () => Get.off(
+                ScanQrPage(),
+                duration: Duration(milliseconds: 500),
+              ),
+              child: Text('Pesan Sekarang'),
+            ),
+          )
+        ],
       ),
     );
   }
